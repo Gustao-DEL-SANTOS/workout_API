857c16208a9302183a4def7852c1ee8e2631899d
Gustavo <63751042+Gustao-DEL-SANTOS@users.noreply.github.com>
2024-07-02 11:38:28 -0300

    config controll de atletas

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -2,19 +2,20 @@ from typing import Annotated
 
 from pydantic import BaseModel, Field, PositiveFloat
 
+from workout_api.categorias.schemas import CategoriaIn
+from workout_api.centro_treinamento.schemas import CentroTreinamento
 from workout_api.contrib.schemas import OutMixin
 
 
 class Atleta(BaseModel):
-    nome: Annotated[
-        str, Field(description="NOME do atleta", example="Joao", max_length=50)
-    ]
-    cpf: Annotated[str, Field(description="CPF do atleta", example="12345678900", max_length=11)]
+    nome: Annotated[str, Field(description="NOME do atleta", example="Joao", max_length=50)]
+    cpf: Annotated[str, Field(description="CPF do atleta", example="12345678900",max_length=11)]
     idade: Annotated[int, Field(description="IDADE do atleta", example="25")]
     peso: Annotated[PositiveFloat, Field(description="PESO do atleta", example="75.5")]
     altura: Annotated[PositiveFloat, Field(description="ALTURA do atleta", example="1.69")]
     sexo: Annotated[str, Field(description="SEXO do atleta", example="M", max_length=1)]
-
+    categoria: Annotated[CategoriaIn, Field(description="Categoria do atleta")]
+    centro_treinamento: Annotated[CentroTreinamento, Field(description="CentroTreinamento do atleta")]
 
 
 class AtletaIn(BaseModel):
