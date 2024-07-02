01e15d8143c9a58c2db8d42ee76bd703e660b37e
Gustavo <63751042+Gustao-DEL-SANTOS@users.noreply.github.com>
2024-07-02 13:31:36 -0300

    projeto da API finalizado

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,10 +1,10 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from pydantic import BaseModel, Field, PositiveFloat
 
 from workout_api.categorias.schemas import CategoriaIn
 from workout_api.centro_treinamento.schemas import CentroTreinamento
-from workout_api.contrib.schemas import OutMixin
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 
 class Atleta(BaseModel):
@@ -18,8 +18,14 @@ class Atleta(BaseModel):
     centro_treinamento: Annotated[CentroTreinamento, Field(description="CentroTreinamento do atleta")]
 
 
-class AtletaIn(BaseModel):
+class AtletaIn(Atleta):
     pass
 
-class AtletaOut(AtletaIn, OutMixin):
+class AtletaOut(Atleta, OutMixin):
     pass
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[Optional[str], Field(None, description="NOME do atleta", example="Joao", max_length=50)]
+    cpf: Annotated[Optional[str], Field(None, description="CPF do atleta", example="12345678900",max_length=11)]
+    idade: Annotated[Optional[int], Field(None, description="IDADE do atleta", example="25")]
+    
